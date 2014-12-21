ccef42416e6c59499de78998744601f35933f31e
microelly2 <thomas@freecadbuch.de>
2014-12-21 21:56:00 +0100

    inspector added

---
+++ Inspector.py
@@ -0,0 +1,16 @@
+import FreeCAD
+import FreeCADGui
+App=FreeCAD
+
+def say(s):
+		FreeCAD.Console.PrintMessage(str(s)+"\n")
+
+
+
+
+def step(now=0):
+#		say("inspector")
+#		say(App.ActiveDocument.Sketch.Geometry[3].length())
+	pass
+	
+
