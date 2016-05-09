25e35e8b94f0a9ffc06d3bf85a0eb6ff7c355166
microelly2 <thomas@freecadbuch.de>
2016-05-09 14:31:31 +0200

    say.py

---
+++ say.py
@@ -0,0 +1,63 @@
+
+import FreeCAD
+
+import FreeCADGui
+App=FreeCAD
+Gui=FreeCADGui
+
+import PySide
+from PySide import QtCore, QtGui
+
+import FreeCAD
+import Draft, Part, Animation
+
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import cm 
+
+import os,random,time,sys,traceback
+
+
+def log(s):
+	logon = False
+	if logon:
+		f = open('/tmp/log.txt', 'a')
+		f.write(str(s) +'\n')
+		f.close()
+
+def sayd(s):
+	if hasattr(FreeCAD,'animation_debug'):
+		pass
+		log(str(s))
+		FreeCAD.Console.PrintMessage(str(s)+"\n")
+
+def say(s):
+	log(str(s))
+	FreeCAD.Console.PrintMessage(str(s)+"\n")
+
+def sayErr(s):
+	log(str(s))
+	FreeCAD.Console.PrintError(str(s)+"\n")
+
+
+def sayW(s):
+	log(str(s))
+	FreeCAD.Console.PrintWarning(str(s)+"\n")
+
+
+def errorDialog(msg):
+    diag = QtGui.QMessageBox(QtGui.QMessageBox.Critical,u"Error Message",msg )
+    diag.setWindowFlags(PySide.QtCore.Qt.WindowStaysOnTopHint)
+    diag.exec_()
+
+
+def sayexc(mess=''):
+	exc_type, exc_value, exc_traceback = sys.exc_info()
+	ttt=repr(traceback.format_exception(exc_type, exc_value,exc_traceback))
+	lls=eval(ttt)
+	l=len(lls)
+	l2=lls[(l-3):]
+	FreeCAD.Console.PrintError(mess + "\n" +"-->  ".join(l2))
+
+
